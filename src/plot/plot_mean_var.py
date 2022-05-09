55e744bf423fdab4673a5b27a6801d4367e58cf8
Yeowon <yeowon010306@g.skku.edu>
2022-05-09 19:51:41 +0900

    code cleaning

--- src/plot/plot_mean_var.py
+++ src/plot/plot_mean_var.py
@@ -4,6 +4,7 @@ from datetime import date
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from stable_baselines3.common.results_plotter import ts2xy
 
 from options import MAPPER_X, MAPPER_Y, get_args
 
@@ -14,47 +15,46 @@ def plot_mean_var(args):
     """
 
     date_today = date.today().isoformat()
+    prop_cycle = plt.rcParams['axes.prop_cycle']
+    colors = prop_cycle.by_key()['color']
 
-    file_paths = []
-    for agent in args.agents:
-        get_path = args.data_path
-        if get_path is None:
-            get_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir, 'data'))
+    get_path = args.data_path
+    if get_path is None:
+        get_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir, 'data'))
+    
+    file_paths = []      
     for (root_dir, _, files) in os.walk(os.path.join(get_path, args.env)):
         for file in files:
             if "0.monitor.csv" in file:
                 file_path = os.path.join(root_dir, file)
                 file_paths.append(file_path)
-
+                
     agent_list = args.agents
-    y_idx, y_name = MAPPER_Y[args.y]
+    _, x_name = MAPPER_X[args.x]
+    _, y_name = MAPPER_Y[args.y]
 
-    for agent in agent_list:
+    for color, agent in zip(colors, agent_list):
         bundle = []
         for file in file_paths:
             if agent in file:
                 df = pd.read_csv(file, skiprows=1)
                 bundle.append(df)
-        
-        df_concat = pd.concat(bundle)  
-        
-        if args.x == 'timesteps':
-            x_var = np.cumsum(df.l.values)
-            y_var = df.r.values
-        elif args.x == 'episode':
-            x_var = np.arange(len(df))
-            y_var = df.r.values
-        elif args.x == 'walltime':
-            # Convert to hours
-            x_var = df.t.values / 3600.0
-            y_var = df.r.values
+                x_var, y_var = ts2xy(df, x_name)
 
-        
-        mean_df = df_concat.groupby(df_concat.index).mean()
-        std_df = df_concat.groupby(df_concat.index).std()
+        l_list = list(map(lambda x: x.l.cumsum(), bundle))
+        x_val = set()
+        for l in l_list:
+            x_val = x_val.union(set(l.values))
+        x_val = sorted(list(x_val))
+        y_val = []
+        for r, l in zip(bundle, l_list):
+            y_val.append(np.interp(x_val, l, r.r))
+        y_val = np.vstack(y_val)
+        y_val_mean = y_val.mean(axis=0)
+        y_val_std = np.std(y_val,axis=0)
 
-        plt.plot(mean_df.iloc[:,y_idx], label = agent)
-        plt.fill_between(mean_df.index, (mean_df-std_df).iloc[:,y_idx], (mean_df+std_df).iloc[:,y_idx], alpha = 0.5)
+        plt.plot(x_val,y_val_mean, label=str(agent)+'-mean', color=color)
+        plt.fill_between(x_val, (y_val_mean-y_val_std), (y_val_mean+y_val_std), alpha = 0.5)
 
     plt.xlabel(args.x)
     plt.ylabel(y_name)
