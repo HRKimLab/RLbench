c7ad96f5a516a917bf50f927f6aa4db2637f5462
HyungGoo <sshs1995@gmail.com>
2022-05-13 18:36:07 +0900

    remove scripts

--- scripts/mount_nfs.bash
+++
@@ -1,4 +0,0 @@
-# make mount point if not exists
-sudo mkdir -p /nfs/share
-# mount nfs (nfs server need to allow this computer)
-sudo mount 10.201.185.92:/volume1/neurlab-ann /nfs/share/
--- scripts/rsync_all.bash
+++
@@ -1,1 +0,0 @@
-rsync -avzh  neurlab-ann@10.201.185.92:/volume1/neurlab-ann/sys_monitor/ /home/neurlab-dl4/sys_monitor/
