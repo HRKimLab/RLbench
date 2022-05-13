402b697a82ff2b0258768d7fcef223ab412c2643
HyungGoo <sshs1995@gmail.com>
2022-05-13 18:55:38 +0900

    routine update

--- scripts/mount_nfs.bash
+++ scripts/mount_nfs.bash
@@ -1,3 +1,11 @@
+# make sure that uid is 1000
+if [ $UID -eq 1000 ]
+then
+	echo "uid is 1000"
+else
+	echo "uid is $UID. please change uid to 1000"
+	exit -1
+fi
 # make mount point if not exists
 sudo mkdir -p /nfs/share
 # mount nfs (nfs server need to allow this computer)
