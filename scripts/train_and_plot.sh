adca949bbd756442cbc3d682bdb96ef73ccb1868
hyeeiin <jgheon.kim@gmail.com>
2022-08-24 12:27:04 +0900

    <fix> run normal dqn in sample script

--- scripts/train_and_plot.sh
+++ scripts/train_and_plot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python train.py --env CartPole-v1 --algo custom_dqn --hp default/dqn --nseed 1 --nstep 1000
-python plot/plot_mean_combined.py --env CartPole-v1 --agents ['a1s1r1'] --x timesteps --y rew 
+python train.py --env CartPole-v1 --algo dqn --hp default/dqn --nseed 3 --nstep 100000
+python plot/plot_mean_combined.py --env CartPole-v1 --agents a1s1 --x timesteps --y rew --window_size 100
